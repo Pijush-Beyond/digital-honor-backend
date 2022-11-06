9be003f40620c6861618472e6e9f64d3d977259b
Pijush Barman <pijushbarman@Pijushs-MacBook-Air.local>
2022-11-07 03:37:22 +0530

    cookie add way changed

--- src/main/java/com/tweetapp/api/controller/TweetAppController.java
+++ src/main/java/com/tweetapp/api/controller/TweetAppController.java
@@ -11,7 +11,9 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.ui.Model;
@@ -83,9 +85,18 @@ public class TweetAppController {
 			System.out.print(authUser);
 			if (authUser != null) {
 				request.getSession().setAttribute("user", user.getUsername());
-				Cookie authCookie = new Cookie("authCookie", authUser.getToken());
-				authCookie.setPath("/");
-				response.addCookie(authCookie);
+				// Cookie authCookie = new Cookie("authCookie", authUser.getToken()+";SameSite=None");
+				// authCookie.setPath("/");
+				// // authCookie.setDomain(".amazonaws.com");
+				// authCookie.setSecure(false);
+				// response.addCookie(authCookie);
+				ResponseCookie cookie = ResponseCookie.from("authCookie", authUser.getToken())
+												.sameSite("None")
+												.path("/")
+												.secure(true)
+												.domain(request.getHeader(HttpHeaders.HOST).split(":")[0])
+												.build();
+				response.setHeader(HttpHeaders.SET_COOKIE, cookie.toString());
 				return new ResponseEntity<UserResponse>(authUser, HttpStatus.OK);
 			} else {
 				response.addCookie(new Cookie("authCookie", null)); // on unsucessfull login remove auth Cookie
